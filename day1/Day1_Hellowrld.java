a6846dba50111915d19775bb4d6d30d1b3e2ff0c
XUANHAU19 <hau9639459@gmail.com>
2025-10-20 19:16:02 +0700

    Day1 - Hello Java world

---
+++ day1/Day1_Hellowrld.java
@@ -0,0 +1,6 @@
+public class Day1_Hellowrld{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hôm nay là ngày đầu tiên trong hành trình học lập trình của tôi.");
+    }
+}
